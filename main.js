5438491333cdcf462513c0b3bd2e88a59578b3c9
jpark6 <qkstjr09@gmail.com>
2021-12-25 14:36:16 +0900

    Feat: make config.json & loadURL

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const env = process.env.NODE_ENV || 'development'
+const config = require('./config/config')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    show: false
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+  if(config.title) {
+    document.getElementsByTagName('title')[0].textContent = config.title   
+  }
+
+  if(config.url) {
+    mainWindow.loadURL(config.url);
+  }
+
+  if(config.isFullScreen) {
+    mainWindow.maximize()
+  } else {
+    const width = config.width || 800;
+    const height = config.height || 800;
+    mainWindow.setSize(width, height)
+  }
 }
 
 // This method will be called when Electron has finished
@@ -41,3 +52,11 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// Development Hot Reload
+if (env === 'development') {
+  require('electron-reload')(__dirname,
+    { electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    hardResetMethod: 'exit'
+  });
+}

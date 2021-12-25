ca15775d506afde3e2815dcac8c0293c3bfeee64
jpark6 <qkstjr09@gmail.com>
2021-12-25 17:26:02 +0900

    Chore: fix typo and require order

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const env = process.env.NODE_ENV || 'development'
 const config = require('./config/config')
+const env = process.env.NODE_ENV || 'development'
 
 function createWindow () {
   // Create the browser window.
